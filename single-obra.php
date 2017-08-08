3bc76a84df8d92bf537847ec0f05c65ea5ff6389
maugelves <mgelves@gmail.com>
2017-08-08 20:25:17 +0200

    Added: Actors to the tech details

--- single-obra.php
+++ single-obra.php
@@ -23,7 +23,7 @@
 
 <section class="sgcta">
 
-    <div class="sgcta__wrapper wrapper">
+    <div class="wrapper">
 
         <h2 class="sgcta__h">Sobre "<?php echo $obra->getTitle(); ?>"</h2>
         <p class="sgcta__b"><?php echo $obra->getShortDescription(); ?></p>
@@ -50,6 +50,89 @@
     </div>
 
 </section>
+
+
+
+<section class="sgtechd">
+
+    <div class="sgcontent__wrapper wrapper">
+
+        <h2 class="sgtechd__h">Ficha Técnica</h2>
+
+        <ul class="sgtechd__items">
+
+
+            <?php // DATE RELEASE ?>
+            <?php if( !empty( $obra->getDateRelease() ) ): ?>
+            <li class="sgtechd__item">
+                <h3 class="sgtechd__item__h">Fecha de estreno:</h3>
+                <p class="sgtechd__item__b"><?php echo $obra->getDateRelease(); ?></p>
+            </li>
+            <?php endif; ?>
+
+
+
+	        <?php // MAIN AND SECONDARY ACTORS ?>
+	        <?php
+                if(
+                        !empty( $obra->getMainActors() )
+                        || !empty( $obra->getSecondaryActors() )
+                ): ?>
+                <li class="sgtechd__item">
+                    <h3 class="sgtechd__item__h">Elenco:</h3>
+                    <p class="sgtechd__item__b">
+                        <?php
+
+                        // Variables
+                        $countMainActors        = count( $obra->getMainActors() );
+                        $countSecondaryActors   = count( $obra->getSecondaryActors() );
+                        $i = 1;
+
+                        // RENDER THE MAIN ACTORS
+                        foreach( $obra->getMainActors() as $actor ): /** @var $actor \MGTC\Models\Actor */ ?>
+
+	                        <?php
+                            // Print the comma in the link?
+                            if (    $countMainActors != $i
+                                    || $countSecondaryActors > 0
+                                ):
+	                            $nombre = $actor->getNombre() . ", ";
+                            else:
+                                $nombre = $actor->getNombre();
+                            endif;
+                            ?>
+                            <a href="<?php echo get_permalink( $actor->getID() ); ?>"><?php echo $nombre; ?></a>
+                            <?php $i++; ?>
+
+                        <?php endforeach; ?>
+
+
+	                    <?php
+	                    // RENDER THE SECONDARY ACTORS
+	                    $i = 1;
+	                    foreach( $obra->getSecondaryActors() as $actor ): /** @var $actor \MGTC\Models\Actor */ ?>
+
+		                    <?php
+		                    // Print the comma in the link?
+                            $nombre = ( $countSecondaryActors != $i ) ? $actor->getNombre() . ", " : $actor->getNombre(); ?>
+                            <?php echo $nombre; ?>
+		                    <?php $i++; ?>
+
+	                    <?php endforeach; ?>
+
+                    </p>
+                </li>
+	        <?php endif; ?>
+
+
+
+        </ul>
+    </div>
+
+</section>
+
+
+
 
 
 <?php get_footer(); ?>
